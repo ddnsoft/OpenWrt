94441b362b3d0e31488b407a0102ab4dd5532b3a
ddnsoft <75249507+ddnsoft@users.noreply.github.com>
2026-01-20 14:52:03 +0800

    change passwall
    Comment out old passwall package clones and add new sources.

--- diy-mini.sh
+++ diy-mini.sh
@@ -39,9 +39,11 @@ git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-a
 
 # 科学上网插件
 ##git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
-git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+##git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+##git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 ##git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main
+src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 # Update Go Version
